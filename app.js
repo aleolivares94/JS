d3c7aae69e3eb55892805c460791fd7f686556d5
Ale Olivares <ale_ema94@hotmail.com>
2021-09-18 09:33:19 -0300

    Clase 1

---
+++ app.js
@@ -0,0 +1,14 @@
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido")
+
+console.log(`Bienvenido a tu calculadora virtual ${nombre} ${apellido}`)
+
+let numero1 = Number(prompt("Ingresa tu numero"));
+let numero2 = Number(prompt("Ingresa tu numero"));
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+alert("Aqui puede ver los resultados de tus numeros con la suma,resta,multiplicacion y division. Gracias!!")

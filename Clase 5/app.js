8c2f6061ba795f849e39437680b6360462ebc7b6
Ale Olivares <ale_ema94@hotmail.com>
2021-10-03 22:24:47 -0300

    Clase 5

---
+++ Clase 5/app.js
@@ -0,0 +1,22 @@
+class Compra {
+    constructor(producto, marca, precio) {
+        this.producto = producto,
+            this.marca = marca,
+            this.precio = precio
+
+    }
+
+    Items() {
+        return `compraste una ${this.producto} marca ${this.marca} te salio ${this.precio}.Gracias!!`
+    }
+
+
+}
+
+const prod = prompt('Ingrese un producto que desee comprar:')
+const mar = prompt('Ingrese la marca')
+const pre = Number(prompt('Coloque el precio'))
+
+const pedido1 = new Compra(prod, mar, pre);
+
+console.log(pedido1.Items());
